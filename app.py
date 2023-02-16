420414597dcb13cd4d26cadd66731ab7eef49385
granthicks <grant.hicks00@gmail.com>
2023-02-15 23:04:46 -0500

    Fleshed out more with argv

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
+import sys
 from api_key import api_key
 import requests
 
-city_name = input("Enter city name: ")
+try:
+    city_name = sys.argv[1]
+except IndexError:
+    city_name = input("Enter city name: ")
 
 weather_data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=imperial")
 
-weather_json = weather_data.json()
+if weather_data.status_code != 200:
+    print(f"Unable to find weather information for {city_name}.")
+else:
+    weather_json = weather_data.json()
 
-descrip = weather_json['weather'][0]['main']
-temp = weather_json['main']['temp']
+    descrip = weather_json['weather'][0]['main']
+    temp = round(weather_json['main']['temp'])
 
-print(f"The weather in {city_name} is {descrip} with a temperature of {temp}F.")
+    print(f"The weather in {city_name} is {descrip} with a temperature of {temp}°F.")
