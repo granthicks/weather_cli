4cf518d39d4480ad2367ab91436168145f0749ce
granthicks <grant.hicks00@gmail.com>
2023-02-26 23:07:09 -0500

    Restructured

--- app.py
+++ app.py
@@ -2,19 +2,50 @@ import sys
 from api_key import api_key
 import requests
 
-try:
-    city_name = sys.argv[1]
-except IndexError:
-    city_name = input("Enter city name: ")
+def get_location_name():
+    """Obtains name of location to look up weather info"""
+    if len(sys.argv) > 1:
+        return sys.argv[1]
+    else:
+        city_name = input("Enter city name: ")
+        return city_name
 
-weather_data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=imperial")
+def get_json_data(location, api_key=api_key):
+    """Looks up weather information for the city and returns json data"""
+    status = False
+    tries = 0
 
-if weather_data.status_code != 200:
-    print(f"Unable to find weather information for {city_name}.")
-else:
-    weather_json = weather_data.json()
+    while not status:
+        weather_data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}&units=imperial")
 
+        if weather_data.status_code != 200 and tries < 3:
+            print(f"Unable to find weather information for {location}.")
+            tries += 1
+            location = input("Enter a city name: ")
+        elif tries == 3:
+            print("Try again later.")
+            quit()
+        else:
+            status = True
+
+    return weather_data.json()
+
+def weather_report(city_name, weather_json):
+    """Prepares the weather report printout string"""
     descrip = weather_json['weather'][0]['main']
     temp = round(weather_json['main']['temp'])
+    return f"The weather in {city_name} is {descrip} with a temperature of {temp}°F."
 
-    print(f"The weather in {city_name} is {descrip} with a temperature of {temp}°F.")
+
+def run():
+    location_name = get_location_name()
+
+    weather_data = get_json_data(location_name)
+
+    report = weather_report(location_name, weather_data)
+
+    print(report)
+
+
+if __name__ == '__main__':
+    run()
